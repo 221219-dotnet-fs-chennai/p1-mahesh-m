fa37ba8c54685597939d7ffe1fef94b9194c68e5
themonk-e <codenmad@gmail.com>
2022-12-20 18:34:07 +0530

    First Assignment

---
+++ assignment/Activity1.sh
@@ -0,0 +1,7 @@
+#! 
+echo "The Vanquisher's Den welcomes you"
+read -p "Please enter your first name" firstName
+read -p "Please enter your last name" lastName
+read -p "Please enter your DoB" DoB
+echo "Hey $firstName $lastName"
+echo -e "\nHere is your date of birth $DoB"
