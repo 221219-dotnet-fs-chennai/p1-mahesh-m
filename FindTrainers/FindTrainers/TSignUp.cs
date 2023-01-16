ec3f126bfb046ab61676625e179ca04da717597c
themonk-e <codenmad@gmail.com>
2023-01-16 17:37:40 +0530

    Signup regex updated

--- FindTrainers/FindTrainers/TSignUp.cs
+++ FindTrainers/FindTrainers/TSignUp.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Datafile;
 namespace FindTrainers
@@ -9,7 +10,12 @@ namespace FindTrainers
     internal class TSignUp : IMenu
     {
         internal static TrainerDetails trainer = new TrainerDetails();
-
+        string? nameRegex = @"\w{3,50}";
+        string? emailRegex = @"^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$";
+        string? phoneRegex = @"^[9876]\d{9}$";
+        string? yearRegex= @"^\d{4}$";
+        string? degreeRegex = @"^B\.\w{1,6}$";
+        string? passwordRegex = @"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$";
         public TSignUp(TrainerDetails newTrainer) {
         trainer=newTrainer;
         }
@@ -50,6 +56,8 @@ namespace FindTrainers
 
         public string UserChoice()
         {
+           
+
             string? userInput = Console.ReadLine();
 
             switch (userInput)
@@ -63,59 +71,145 @@ namespace FindTrainers
 
                 case "2":
                     Console.WriteLine("Enter First Name");
-                    trainer.FName = Console.ReadLine();
+                    string str = Console.ReadLine();
+                    while (!FormatChecker(str,nameRegex))
+                    {
+                       Console.WriteLine("First Name should be 3 to 50 characters long");
+                        str = Console.ReadLine();
+                    }
+                    trainer.FName = str;
+
                     return "TSignUp";
                 case "3":
                     Console.WriteLine("Enter Last Name");
-                    trainer.LName = Console.ReadLine();
+                     str = Console.ReadLine();
+                    while (!FormatChecker(str,nameRegex))
+                    {
+                        Console.WriteLine("Last Name should be 3 to 50 characters long");
+                        str = Console.ReadLine();
+                    }
+                    trainer.LName = str;
                     return "TSignUp";
                 case "4":
                     Console.WriteLine("Enter Email ID");
-                    trainer.Email = Console.ReadLine();
+                    str= Console.ReadLine();
+                    while (!FormatChecker(str,emailRegex))
+                    {
+                        Console.WriteLine("Email format is invalid! Re-enter email");
+                        str = Console.ReadLine();
+                    }
+                    trainer.Email= str; 
+
                     return "TSignUp";
                 case "5":
                     Console.WriteLine("Enter Phone No.");
-                    trainer.PhoneNo = Console.ReadLine();
+                    str = Console.ReadLine();
+                    while (!FormatChecker(str, phoneRegex))
+                    {
+                        Console.WriteLine("Phone No. format is invalid! Re-enter Phone No.");
+                        str = Console.ReadLine();
+                    }
+                    trainer.PhoneNo= str;   
                     return "TSignUp";
                 case "6":
                     Console.WriteLine("Enter Bachelor's College Name");
-                    trainer.UGCName = Console.ReadLine();
+                    str = Console.ReadLine();
+                    while (!FormatChecker(str, nameRegex))
+                    {
+                        Console.WriteLine("College Name should be 3 to 50 characters long");
+                        str = Console.ReadLine();
+                    }
+                    trainer.UGCName= str;
                     return "TSignUp";
                 case "7":
                     Console.WriteLine("Enter Bachelor's Year of Passing");
-                    trainer.UGPYear = Console.ReadLine();
+                     str = Console.ReadLine();
+                    while (!FormatChecker(str, yearRegex) || int.Parse(str)>2023)
+                    {
+                        Console.WriteLine("Re-enter the correct year!");
+                        str = Console.ReadLine();
+                    }
+                    trainer.UGPYear = str;
                     return "TSignUp";
                 case "8":
-                    Console.WriteLine("Enter Bachelor's Degree");
-                    trainer.UGDegree = Console.ReadLine();
+                    Console.WriteLine("Enter Bachelor's Degree (Format eg: B.Tech,B.A,B.Sc");
+                    str = Console.ReadLine();
+                    while (!FormatChecker(str,degreeRegex ))
+                    {
+                        Console.WriteLine("Please use the correct format for degree");
+                        str = Console.ReadLine();
+                    }
+                    trainer.UGDegree= str;
+
                     return "TSignUp";
                 case "9":
                     Console.WriteLine("Enter Bachelor's Specialization");
-                    trainer.UGDept = Console.ReadLine();
+                    str= Console.ReadLine();
+                    while (!FormatChecker(str, nameRegex))
+                    {
+                        Console.WriteLine("Specializtion should be 3 to 50 characters long");
+                        str = Console.ReadLine();
+                    }
                     return "TSignUp";
                 case "10":
                     Console.WriteLine("Enter HigherSec School Name");
-                    trainer.HSCName = Console.ReadLine();
+                    str= Console.ReadLine();
+                    while (!FormatChecker(str, nameRegex))
+                    {
+                        Console.WriteLine("Higher Secondary School Name should be 3 to 50 characters long");
+                        str = Console.ReadLine();
+                    }
+                    trainer.HSCName= str;
                     return "TSignUp";
                 case "11":
                     Console.WriteLine("Enter HigherSec School Year of Passing");
-                    trainer.HSCPYear = Console.ReadLine();
+                    str= Console.ReadLine();
+                    while (!FormatChecker(str, yearRegex) || int.Parse(str) > 2023)
+                    {
+                        Console.WriteLine("Re-enter the correct year!");
+                        str = Console.ReadLine();
+                    }
+                    trainer.HSCPYear= str;
                     return "TSignUp";
                 case "12":
                     Console.WriteLine("Enter HigherSec School Stream");
-                    trainer.HSCStream = Console.ReadLine();
+                    str= Console.ReadLine();
+                    str = Console.ReadLine();
+                    while (!FormatChecker(str, nameRegex))
+                    {
+                        Console.WriteLine("Higher Secondary Stream should be 3 to 50 characters long");
+                        str = Console.ReadLine();
+                    }
                     return "TSignUp";
                 case "13":
                     Console.WriteLine("Enter High School Name");
-                    trainer.HSName = Console.ReadLine();
+                    str = Console.ReadLine();
+                    while (!FormatChecker(str, nameRegex))
+                    {
+                        Console.WriteLine("High School Name should be 3 to 50 characters long");
+                        str = Console.ReadLine();
+                    }
+                    trainer.HSName= str;
                     return "TSignUp";
                 case "14":
                     Console.WriteLine("Enter High School Year of Passing");
-                    trainer.HSPYear = Console.ReadLine();
+                    str = Console.ReadLine();
+                    while (!FormatChecker(str, yearRegex) || int.Parse(str) > 2023)
+                    {
+                        Console.WriteLine("Re-enter the correct year!");
+                        str = Console.ReadLine();
+                    }
+                    trainer.HSPYear= str;   
                     return "TSignUp";
                 case "15":
                     Console.WriteLine("Enter Last Company Name");
-                    trainer.LastCompany = Console.ReadLine();
+                    str= Console.ReadLine();
+                    while (!FormatChecker(str, nameRegex))
+                    {
+                        Console.WriteLine("Last Company name should be 3 to 50 characters long");
+                        trainer.LastCompany = Console.ReadLine();
+                    }
+                    trainer.HSName = str;
                     return "TSignUp";
                 case "16":
                     Console.WriteLine("Enter total experience in years");
@@ -123,23 +217,60 @@ namespace FindTrainers
                     return "TSignUp";
                 case "17":
                     Console.WriteLine("Enter your Primary Skill");
-                    trainer.Skill1 = Console.ReadLine();
+                    str= Console.ReadLine();
+                    while (!FormatChecker(str, nameRegex))
+                    {
+                        Console.WriteLine("Primary Skill should be 3 to 50 characters long");
+                        str = Console.ReadLine();
+                    }
+                    trainer.Skill1 = str;
                     return "TSignUp";
                 case "18":
                     Console.WriteLine("Enter your Secondary Skill");
-                    trainer.Skill2 = Console.ReadLine();
+                    str = Console.ReadLine();
+                    while (!FormatChecker(str, nameRegex))
+                    {
+                        Console.WriteLine("Secondary Skill should be 3 to 50 characters long");
+                        str = Console.ReadLine();
+                    }
+                    trainer.Skill2= str;
                     return "TSignUp";
                 case "19":
                     Console.WriteLine("Enter your Tertiary Skill");
-                    trainer.Skill3 = Console.ReadLine();
+                    str = Console.ReadLine();
+                    while (!FormatChecker(str, nameRegex))
+                    {
+                        Console.WriteLine("Tertiary Skill should be 3 to 50 characters long");
+                        str = Console.ReadLine();
+                    }
+                    trainer.Skill3 = str;
                     return "TSignUp";
                 case "20":
                     Console.WriteLine("Enter your Quaternary Skill");
-                    trainer.Skill4 = Console.ReadLine();
+                    str = Console.ReadLine();
+                    while (!FormatChecker(str, nameRegex))
+                    {
+                        Console.WriteLine("Quarternary Skill should be 3 to 50 characters long");
+                        str = Console.ReadLine();
+                    }
+                    trainer.Skill4 = str;
                     return "TSignUp";
                 case "21":
                     Console.WriteLine("Enter your Password");
-                    trainer.Password = Console.ReadLine();
+                    str = Console.ReadLine();
+                    while (!FormatChecker(str, passwordRegex))
+                    {
+                        Console.WriteLine("Password min 8 characters, atleast 1 lower case,1 upper case,1 number");
+                        str = Console.ReadLine();
+                    }
+                    Console.WriteLine("Retype password to Confirm Password");
+                    string cPass = Console.ReadLine();
+                    while (!str.Equals(cPass))
+                    {
+                        Console.WriteLine("Passwords are not matching! Re type confirm password! ");
+                        cPass = Console.ReadLine();
+                    }
+
                     return "TSignUp";
 
                 default:
@@ -149,6 +280,22 @@ namespace FindTrainers
                     return "TSignUp";
 
             }
+        }
+        
+        public bool FormatChecker(string str,string regex)
+        {
+            Regex r = new Regex(regex);
+            if (r.IsMatch(str))
+            {
+                return true;
+
+            }
+            else
+            {
+                return false;
+            }
+
+          
         }
     }
 }
