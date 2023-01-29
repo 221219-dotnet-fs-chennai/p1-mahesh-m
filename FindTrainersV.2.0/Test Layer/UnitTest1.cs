08571cffbc62bea88ad1b23bd51551f39cf1146a
themonk-e <codenmad@gmail.com>
2023-01-29 19:51:13 +0530

    [test_for_checking_IsExit_Phoneno_working ]Works

--- FindTrainersV.2.0/Test Layer/UnitTest1.cs
+++ FindTrainersV.2.0/Test Layer/UnitTest1.cs
@@ -1,7 +1,9 @@
+using BusinessLogic;
 namespace Test_Layer
 {
     public class Tests
     {
+        IRepo repo = new EFRepo();
         [SetUp]
         public void Setup()
         {
@@ -11,6 +13,29 @@ namespace Test_Layer
         public void Test1()
         {
             Assert.Pass();
+        }
+
+
+        [Test]
+        public void test_for_checking_IsExit_Phoneno_working ()
+        {
+            Dictionary<string,bool> phoneNos= new Dictionary<string,bool>();
+            phoneNos.Add("8918178911", true);
+            phoneNos.Add("9010111000", true);
+            phoneNos.Add("9361098885", false);
+            phoneNos.Add("9000011111", false);
+
+            List<string> ph = new List<string>();
+            ph.Add("8918178911");
+            ph.Add("9010111000");
+            ph.Add("9361098885");
+            ph.Add("9000011111");
+            Random random= new Random();
+            int randomNo = random.Next(0, 4);
+
+            bool res = repo.IsExist(ph[randomNo],"phoneNo");
+
+            Assert.That(res, Is.EqualTo(phoneNos[ph[randomNo]]));
         }
     }
 }
