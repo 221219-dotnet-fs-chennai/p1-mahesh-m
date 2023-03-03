6a032f608cd9b7e90a155802188060d4eb1a7791
themonk-e <codenmad@gmail.com>
2023-03-03 19:44:21 +0530

    code1

---
+++ codes/MergeSortedArrays.cs
@@ -0,0 +1,58 @@
+﻿class Question
+{
+    public static void Main(string[] args)
+    {
+        int[] arr = {1,4,8,10,15,30};
+        int[] arr2 = {2,3,7,9,12,13};   
+
+        int []result=Merge(arr,arr2);
+
+        foreach(var e in result)
+        {
+            Console.Write(e+" ");
+        }
+    }
+
+    public static int [] Merge(int[] arr, int[] arr2)
+    {
+        int[] result = new int[arr2.Length+arr.Length];
+
+
+        int n = arr.Length; int m=arr2.Length;
+
+        int i = 0;int j = 0;int k = 0;
+
+        while(i<n && j<m)
+        {
+            Console.WriteLine("Comp" + " " + arr[i] + " " + arr2[j]);
+            if (arr[i] < arr2[j])
+            {
+                result[k] = arr[i];
+                k++;
+                i++;
+            }
+            else
+            {
+                result[k] = arr2[j];
+                k++;
+                j++;
+            }
+        }
+        while (i < n)
+        {
+            result[k] = arr[i];
+            k++;
+            i++;
+        }
+        while(j < m)
+        {
+            result[k] = arr2[j];
+            k++;
+            j++;
+        }
+
+
+
+        return result;
+    }
+}
